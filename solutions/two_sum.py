6fd33d2f20115edb1765cba92c0b9745242798a1
masa08 <ushimasarug08@gmail.com>
2022-12-17 11:21:42 +0900

    add comments to two num

--- solutions/two_sum.py
+++ solutions/two_sum.py
@@ -15,10 +15,13 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}  # { value: index }
 
+        # Traverse the array and store elements in a map
         for index, num in enumerate(nums):
             remain = target - num
+            # if remain exists in a map, return indexs
             if remain in seen:
                 return [seen[remain], index]
+            # if not, store a index and continue
             seen[num] = index
 
 

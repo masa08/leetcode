a9f541ead7011d35a2aae5c7dbbbcdeea5916606
masa08 <ushimasarug08@gmail.com>
2023-02-11 12:45:37 +0900

    add two pointers way

--- solutions/remove_duplicates_from_sorted_list..py
+++ solutions/remove_duplicates_from_sorted_list..py
@@ -30,13 +30,30 @@ def main():
 
 
 class Solution:
-    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        current_node = head
+    # def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     current_node = head
 
-        while current_node:
-            while current_node.next and current_node.val == current_node.next.val:
-                current_node.next = current_node.next.next
-            current_node = current_node.next
+    #     while current_node:
+    #         while current_node.next and current_node.val == current_node.next.val:
+    #             current_node.next = current_node.next.next
+    #         current_node = current_node.next
+
+    #     return head
+
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None:
+            return head
+
+        prev = head
+        current = head.next
+
+        while current:
+            if current.val == prev.val:
+                prev.next = current.next
+                current = current.next
+            else:
+                prev = prev.next
+                current = current.next
 
         return head
 

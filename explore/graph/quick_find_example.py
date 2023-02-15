f44fefbf736de062227dec7ed81a945a548900b1
masa08 <ushimasarug08@gmail.com>
2023-02-15 09:36:32 +0900

    add quick find and quick union

--- explore/graph/quick_find_example.py
+++ explore/graph/quick_find_example.py
@@ -1,10 +1,13 @@
+# quick find
 class UnionFind:
     def __init__(self, size) -> None:
         self.root = [i for i in range(size)]
 
+    # O(1)
     def find(self, x):
         return self.root[x]
 
+    # O(N)
     def union(self, x, y):
         rootX = self.root[x]
         rootY = self.root[y]
@@ -27,6 +30,7 @@ uf.union(5, 6)
 uf.union(6, 7)
 uf.union(3, 8)
 uf.union(8, 9)
+
 print(uf.connected(1, 5))  # true
 print(uf.connected(5, 7))  # true
 print(uf.connected(4, 9))  # false

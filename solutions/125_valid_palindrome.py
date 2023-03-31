679a6d40a0c491fe2e89f0ad5d4c76321fa99fd8
masa08 <ushimasarug08@gmail.com>
2023-03-31 09:18:00 +0900

    update 125

--- solutions/125_valid_palindrome.py
+++ solutions/125_valid_palindrome.py
@@ -7,25 +7,29 @@ def main():
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # parse s and arrange it
         s = s.lower()
-        aplhanumeric = [chr(i) for i in range(97, 97+26)] + \
-            [str(i) for i in range(0, 10)]
-        arranged = ""
-        for i in range(len(s)):
-            if s[i] in aplhanumeric:
-                arranged += s[i]
+        arranged = [c for c in s if c.isalnum()]
+        return arranged == arranged[::-1]
 
-        # check if palindrome
-        left, right = 0, len(arranged)-1
-        while left < right:
-            if arranged[left] == arranged[right]:
-                left += 1
-                right -= 1
-            else:
-                return False
+        # # parse s and arrange it
+        # s = s.lower()
+        # aplhanumeric = [chr(i) for i in range(97, 97+26)] + \
+        #     [str(i) for i in range(0, 10)]
+        # arranged = ""
+        # for i in range(len(s)):
+        #     if s[i] in aplhanumeric:
+        #         arranged += s[i]
 
-        return True
+        # # check if palindrome
+        # left, right = 0, len(arranged)-1
+        # while left < right:
+        #     if arranged[left] == arranged[right]:
+        #         left += 1
+        #         right -= 1
+        #     else:
+        #         return False
+
+        # return True
 
 
 if __name__ == '__main__':

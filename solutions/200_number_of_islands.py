e85d36b0d579c20d43aef55a2d6ed12c288d9c21
masa08 <ushimasarug08@gmail.com>
2023-06-13 09:11:23 +0900

    add number of islands bfs way

--- solutions/200_number_of_islands.py
+++ solutions/200_number_of_islands.py
@@ -5,13 +5,13 @@ def main():
     args = [["1", "1", "1", "1", "0"], ["1", "1", "0", "1", "0"],
             ["1", "1", "0", "0", "0"], ["0", "0", "0", "0", "0"]]
     solution = Solution()
-    result = solution.numIslands(args)
+    result = solution.numIslandsBfs(args)
     print(result)
 
 
 class Solution:
     # dfs
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def numIslandsDfs(self, grid: List[List[str]]) -> int:
         def _dfs(grid, r, c):
             if (r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0])):
                 return
@@ -37,6 +37,35 @@ class Solution:
         return result
 
     # bfs
+    def numIslandsBfs(self, grid: List[List[str]]) -> int:
+        def _bfs(grid, r, c):
+            queue = [[r, c]]
+
+            while len(queue) != 0:
+                r, c = queue.pop(0)
+
+                if (r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0])):
+                    continue
+                if grid[r][c] == "0":
+                    continue
+
+                grid[r][c] = "0"
+                queue.append([r-1, c])
+                queue.append([r+1, c])
+                queue.append([r, c+1])
+                queue.append([r, c-1])
+
+        rows = len(grid)
+        columns = len(grid[0])
+        result = 0
+
+        for r in range(rows):
+            for c in range(columns):
+                if grid[r][c] == "1":
+                    result += 1
+                    _bfs(grid, r, c)
+
+        return result
 
 
 if __name__ == '__main__':

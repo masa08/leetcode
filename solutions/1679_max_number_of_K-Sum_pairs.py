af6bc8bce36809257dfa0e734d0e24d8b571a771
masa08 <ushimasarug08@gmail.com>
2023-07-31 09:37:38 +0900

    :sparkle: update 1679

--- solutions/1679_max_number_of_K-Sum_pairs.py
+++ solutions/1679_max_number_of_K-Sum_pairs.py
@@ -10,19 +10,44 @@ def main():
 
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        count = 0
-        nums = sorted(nums)
-        left, right = 0, len(nums)-1
+        # two pointer way
+        # count = 0
+        # nums = sorted(nums)
+        # left, right = 0, len(nums)-1
 
-        while left < right:
-            if nums[left] + nums[right] == k:
-                count += 1
-                left += 1
-                right -= 1
-            elif nums[left] + nums[right] < k:
-                left += 1
+        # while left < right:
+        #     if nums[left] + nums[right] == k:
+        #         count += 1
+        #         left += 1
+        #         right -= 1
+        #     elif nums[left] + nums[right] < k:
+        #         left += 1
+        #     else:
+        #         right -= 1
+
+        # return count
+
+        # hash map way
+        mapping = {}
+        count = 0
+
+        for n in nums:
+            if n in mapping:
+                mapping[n] += 1
             else:
-                right -= 1
+                mapping[n] = 1
+
+        for i in range(len(nums)):
+            current = nums[i]
+            complement = k - nums[i]
+            isCount = (current in mapping and mapping[current] > 0) and (
+                complement in mapping and mapping[complement] > 0)
+            if isCount:
+                if current == complement and mapping[current] < 2:
+                    continue
+                mapping[current] -= 1
+                mapping[complement] -= 1
+                count += 1
 
         return count
 

8d8caab1b4ee0a0fbe9e3307aa8cde846f74d4ee
masa08 <ushimasarug08@gmail.com>
2023-08-01 09:20:24 +0900

    :sparkle: update 238

--- solutions/238_product_of_array_except_self.py
+++ solutions/238_product_of_array_except_self.py
@@ -10,17 +10,29 @@ def main():
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # length = len(nums)
+        # left, right, answer = [1] * length, [1] * length, [1] * length
+
+        # for i in range(1, length):
+        #     left[i] = nums[i-1] * left[i-1]
+
+        # for i in reversed(range(length-1)):
+        #     right[i] = nums[i+1] * right[i+1]
+
+        # for i in range(length):
+        #     answer[i] = left[i] * right[i]
+
+        # return answer
+
         length = len(nums)
-        left, right, answer = [1] * length, [1] * length, [1] * length
-
+        answer = [1] * length
         for i in range(1, length):
-            left[i] = nums[i-1] * left[i-1]
+            answer[i] = nums[i-1] * answer[i-1]
 
-        for i in reversed(range(length-1)):
-            right[i] = nums[i+1] * right[i+1]
-
-        for i in range(length):
-            answer[i] = left[i] * right[i]
+        r = 1
+        for i in reversed(range(length)):
+            answer[i] = answer[i] * r
+            r *= nums[i]
 
         return answer
 

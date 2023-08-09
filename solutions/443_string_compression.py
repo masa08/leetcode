20b60e15310fa781f00b42e02db3762b2002330f
masa08 <ushimasarug08@gmail.com>
2023-08-10 08:55:20 +0900

    :sparkle: update 443

--- solutions/443_string_compression.py
+++ solutions/443_string_compression.py
@@ -10,24 +10,43 @@ def main():
 
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        index = 0
-        result = 0
+        length = len(chars)
+        if length < 2:
+            return length
 
-        while index < len(chars):
-            group_length = 1
-            while (index+group_length < len(chars) and chars[index+group_length] == chars[index]):
-                group_length += 1
-            chars[result] = chars[index]  # replace the first char in the group
-            result += 1  # move to the next position
+        anchor = 0
+        write = 0
+        for index, char in enumerate(chars):
+            if (index + 1) == length or char != chars[index+1]:
+                chars[write] = char
+                write += 1
+                if index > anchor:
+                    repeated_times = index - anchor + 1
+                    for num in str(repeated_times):
+                        chars[write] = num
+                        write += 1
+                anchor = index + 1
 
-            if group_length > 1:
-                group_length_str = str(group_length)
-                chars[result:result+len(group_length_str)
-                      ] = list(group_length_str)
-                result += len(group_length_str)
-            index += group_length
+        return write
 
-        return result
+        # index = 0
+        # result = 0
+
+        # while index < len(chars):
+        #     group_length = 1
+        #     while (index+group_length < len(chars) and chars[index+group_length] == chars[index]):
+        #         group_length += 1
+        #     chars[result] = chars[index]  # replace the first char in the group
+        #     result += 1  # move to the next position
+
+        #     if group_length > 1:
+        #         group_length_str = str(group_length)
+        #         chars[result:result+len(group_length_str)
+        #               ] = list(group_length_str)
+        #         result += len(group_length_str)
+        #     index += group_length
+
+        # return result
 
 
 if __name__ == '__main__':

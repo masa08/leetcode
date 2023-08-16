44fab119fb1f148802cc13517bc7f115e58ca44e
masa08 <ushimasarug08@gmail.com>
2023-08-16 09:34:50 +0900

    :sparkle: add 1657

---
+++ solutions/1657_determine_if_two_strings_are_close.py
@@ -0,0 +1,39 @@
+def main():
+    solution = Solution()
+    result = solution.closeStrings("abc", "bca")
+    print(result)
+
+
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
+
+        word1_map = dict()
+        word2_map = dict()
+
+        for c in word1:
+            if c in word1_map:
+                word1_map[c] += 1
+            else:
+                word1_map[c] = 1
+
+        for c in word2:
+            if c in word2_map:
+                word2_map[c] += 1
+            else:
+                word2_map[c] = 1
+
+        if word1_map.keys() != word2_map.keys():
+            return False
+
+        word1_frequency_list = list(word1_map.values())
+        word2_frequency_list = list(word2_map.values())
+        word1_frequency_list.sort()
+        word2_frequency_list.sort()
+
+        return word1_frequency_list == word2_frequency_list
+
+
+if __name__ == '__main__':
+    main()

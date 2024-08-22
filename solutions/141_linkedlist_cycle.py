a34b9fdd441bef39aeba7466ec8bfb4f77510b37
masa08 <ushimasarug08@gmail.com>
2024-08-23 08:19:51 +0900

    add 141 floyed's cycle

--- solutions/141_linkedlist_cycle.py
+++ solutions/141_linkedlist_cycle.py
@@ -27,15 +27,28 @@ def main():
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        seen = set()
+        # Hash Table
+        # seen = set()
 
-        while head:
-            if head in seen:
-                return True
-            seen.add(head)
-            head = head.next
+        # while head:
+        #     if head in seen:
+        #         return True
+        #     seen.add(head)
+        #     head = head.next
 
-        return False
+        # return False
+
+        # Floyd's Cycle Finding Algorithm
+        if head is None:
+            return False
+        slow = head
+        fast = head.next
+
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
 
 
 if __name__ == '__main__':

6e3e455ef7d495e7b3465f18ed4ca929c55fa1c7
masa08 <ushimasarug08@gmail.com>
2024-08-24 16:54:23 +0900

    update 104

--- solutions/104_maximum_depth_of_binary_tree.py
+++ solutions/104_maximum_depth_of_binary_tree.py
@@ -8,26 +8,38 @@ class TreeNode:
         self.right = right
 
 
+def makeTree(args):
+    nodes = [TreeNode(val) if val else None for val in args]
+    for i, node in enumerate(nodes):
+        if node:
+            left = i * 2 + 1
+            right = i * 2 + 2
+            if left < len(nodes):
+                node.left = nodes[left]
+            if right < len(nodes):
+                node.right = nodes[right]
+    return nodes[0]
+
+
 def main():
-    # TODO: 生成対応
-    # args = [3, 9, 20, 2, 2, 15, 7]
-    # solution = Solution()
-    # result = solution.maxDepth()
-    # print(result)
+    args = [3, 9, 20, None, None, 15, 7]
+    solution = Solution()
+    result = solution.maxDepth(makeTree(args))
+    print(result)
     pass
 
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        # if not root:
-        #     return 0
-        # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-
         if not root:
             return 0
+
+        # DFS approach
+        # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+        # BFS approach
         queue = [root]
         depth = 0
-
         while queue:
             for _ in range(len(queue)):
                 q = queue.pop(0)

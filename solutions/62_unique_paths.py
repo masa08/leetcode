d37fd735e48ce79586ae84708e39486375982e50
masa08 <ushimasarug08@gmail.com>
2024-08-25 11:37:21 +0900

    update 62

--- solutions/62_unique_paths.py
+++ solutions/62_unique_paths.py
@@ -8,12 +8,18 @@ def main():
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        # Initialize a 2D list (dp) with 1s, representing the number of ways to reach each cell
         dp = [[1] * n for _ in range(m)]
 
+        # Iterate over each cell starting from the second row and second column
+        # because the first row and the first column are already initialized to 1
         for row in range(1, m):
             for col in range(1, n):
+                # The number of ways to reach the current cell is the sum of the ways to reach
+                # the cell directly above it and the cell directly to the left of it
                 dp[row][col] = dp[row-1][col] + dp[row][col-1]
 
+        # Return the number of ways to reach the bottom-right corner of the grid
         return dp[m-1][n-1]
 
 

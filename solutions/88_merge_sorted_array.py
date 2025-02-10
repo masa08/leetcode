449278864bfcd94b1102bcdda5a12af139d0f7f3
masa08 <ushimasarug08@gmail.com>
2025-02-10 09:24:15 +0900

    update 88

--- solutions/88_merge_sorted_array.py
+++ solutions/88_merge_sorted_array.py
@@ -6,7 +6,27 @@ def main():
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        nums1[:] = sorted(nums1[:m] + nums2[:n])
+        # Brute force
+        # for i in range(n):
+        #     nums1[i+m] = nums2[i]
+        
+        # nums1.sort()
+
+        # three pointer
+        nums1Copy = nums1[:m]
+        p1 = 0
+        p2 = 0
+
+        for p in range(m+n):
+            print(p1, p2)
+            if p1 >= m or (p2 < n and nums1Copy[p1] > nums2[p2]):
+                nums1[p] = nums2[p2]
+                p2 += 1
+            else:
+                nums1[p] = nums1Copy[p1]
+                p1 += 1
+            p += 1
+
         
 if __name__ == '__main__':
     main()

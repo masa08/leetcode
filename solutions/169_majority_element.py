94ff8934a1a11dea364c6083152bf1eb22da5409
masa08 <ushimasarug08@gmail.com>
2025-02-12 08:39:32 +0900

    add 169

---
+++ solutions/169_majority_element.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+def main():
+    args = [3, 2, 3]
+    solution = Solution()
+    result = solution.majorityElement(args)
+    print(result)
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        pivod = len(nums)/2
+        counter = {}
+
+        for i in range(len(nums)):
+            if nums[i] in counter:
+                counter[nums[i]] += 1
+            else:
+                counter[nums[i]] = 1
+        
+        for i,v in counter.items():
+            if v >= pivod:
+                return i
+
+
+if __name__ == '__main__':
+    main()

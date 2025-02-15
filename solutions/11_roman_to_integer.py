98743bd5ca2c0256f6d9b4a97d3db3bd99437a05
masa08 <ushimasarug08@gmail.com>
2025-02-15 15:29:07 +0900

    update 11

--- solutions/11_roman_to_integer.py
+++ solutions/11_roman_to_integer.py
@@ -8,25 +8,40 @@ def main():
     print(result)
 
 
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        mapping = {"I": 1, "V": 5, "X": 10,
+mapping = {"I": 1, "V": 5, "X": 10,
                    "L": 50, "C": 100, "D": 500, "M": 1000}
 
-        prev = 0
-        res = 0
 
-        for char in s:
-            val = mapping[char]
-            if prev < val:
-                res -= prev
-                res += val - prev
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        total = 0
+        i = 0
+
+        while i < len(s):
+            if i + 1 < len(s) and mapping[s[i]] < mapping[s[i+1]]:
+                total += mapping[s[i+1]] - mapping[s[i]]
+                i += 2
             else:
-                res += val
+                total += mapping[s[i]]
+                i += 1
 
-            prev = val
+        return total
 
-        return res
+        # My solution
+        # prev = 0
+        # res = 0
+
+        # for char in s:
+        #     val = mapping[char]
+        #     if prev < val:
+        #         res -= prev
+        #         res += val - prev
+        #     else:
+        #         res += val
+
+        #     prev = val
+
+        # return res
 
 
 if __name__ == '__main__':

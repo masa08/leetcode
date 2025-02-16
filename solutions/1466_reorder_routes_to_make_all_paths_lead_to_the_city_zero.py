be1f128bbe7781ba0658ca432886c3dfa0d3d31b
masa08 <ushimasarug08@gmail.com>
2025-02-16 18:46:59 +0900

    add 1466

---
+++ solutions/1466_reorder_routes_to_make_all_paths_lead_to_the_city_zero.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+
+
+def main():
+    args = (6, [[0, 1], [1, 3], [2, 3], [4, 0], [4, 5]])
+    solution = Solution()
+    result = solution.minReorder(*args)
+    print(result)
+
+
+class Solution:
+    def minReorder(self, n: int, connections: list[list[int]]) -> int:
+        graph = defaultdict(list)
+        edges = set()
+
+        for a, b in connections:
+            graph[a].append(b)
+            graph[b].append(a)
+            edges.add((a, b))
+
+        def dfs(city, parent):
+            changes = 0
+            for neighbor in graph[city]:
+                if neighbor == parent:
+                    continue
+
+                if (city, neighbor) in edges:
+                    changes += 1
+
+                changes += dfs(neighbor, city)
+            return changes
+
+        return dfs(0, -1)
+
+
+if __name__ == '__main__':
+    main()

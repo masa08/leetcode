8e07ecd1e31e327c9eed7f42cd638960058618a8
masa08 <ushimasarug08@gmail.com>
2025-02-20 09:01:33 +0900

    update 1161

--- solutions/1161_maximum_level_sum_of_a_binary_tree.py
+++ solutions/1161_maximum_level_sum_of_a_binary_tree.py
@@ -19,27 +19,47 @@ class TreeNode:
 
 class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-        def bfs(root):
-            queue = [root]
-            level = 1
+        queue = [root]
+        max_sum, answer, cur_level = root.val, 1, 1
 
-            while queue:
-                val = 0
-                for _ in range(0, len(queue)):
-                    node = queue.pop(0)
-                    if node.left:
-                        queue.append(node.left)
-                    if node.right:
-                        queue.append(node.right)
-                    val += node.val
+        while queue:
+            total = 0
 
-                level_value[level] = val
-                level += 1
+            for _ in range(0, len(queue)):
+                node = queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                total += node.val
 
-        level_value = defaultdict(int)
-        bfs(root)
+            if max_sum < total:
+                max_sum, answer = total, cur_level
+            cur_level += 1
 
-        return max(level_value, key=level_value.get)
+        return answer
+
+        # def bfs(root):
+        #     queue = [root]
+        #     level = 1
+
+        #     while queue:
+        #         val = 0
+        #         for _ in range(0, len(queue)):
+        #             node = queue.pop(0)
+        #             if node.left:
+        #                 queue.append(node.left)
+        #             if node.right:
+        #                 queue.append(node.right)
+        #             val += node.val
+
+        #         level_value[level] = val
+        #         level += 1
+
+        # level_value = defaultdict(int)
+        # bfs(root)
+
+        # return max(level_value, key=level_value.get)
 
 
 if __name__ == '__main__':

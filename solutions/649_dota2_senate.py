cc5ab34960015e9197374b7dc572d387f67bbaad
masa08 <ushimasarug08@gmail.com>
2025-03-27 08:21:57 +0900

    update 649

--- solutions/649_dota2_senate.py
+++ solutions/649_dota2_senate.py
@@ -32,6 +32,30 @@ class Solution:
 
         return "Radiant" if queue_r else "Dire"
 
+        # We can also use list instead of deque
+        # but deque is faster than list
+        # list pop(0) is O(n) time complexity
+        # On the other hand, deque popleft() is O(1) time complexity
+
+        # queue_r = []
+        # queue_d = []
+        # for i, s in enumerate(senate):
+        #     if s == 'R':
+        #         queue_r.append(i)
+        #     else:
+        #         queue_d.append(i)
+
+        # while queue_r and queue_d:
+        #     r_index = queue_r.pop(0)
+        #     d_index = queue_d.pop(0)
+
+        #     if r_index < d_index:
+        #         queue_r.append(r_index + len(senate))
+        #     else:
+        #         queue_d.append(d_index + len(senate))
+
+        # return "Radiant" if queue_r else "Dire"
+
 
 if __name__ == '__main__':
     main()

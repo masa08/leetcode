881cfb1ee5e5c6295b746a0c453973a886c01795
masa08 <ushimasarug08@gmail.com>
2025-03-31 08:51:21 +0900

    update 198

--- solutions/198_house_robber.py
+++ solutions/198_house_robber.py
@@ -11,18 +11,18 @@ def main():
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
+
         if n <= 2:
             return max(nums)
 
         dp = [0] * n
         dp[0] = nums[0]
-        dp[1] = nums[1]
+        dp[1] = max(nums[0], nums[1])
 
         for i in range(2, n):
-            max_non_adjacent = max(dp[:i-1])
-            dp[i] = nums[i] + max_non_adjacent
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
 
-        return max(dp)
+        return dp[-1]
 
 
 if __name__ == '__main__':

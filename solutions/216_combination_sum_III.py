3479fd6d2abdfb3583d5ddd9f1898bbff4adf393
masa08 <ushimasarug08@gmail.com>
2025-04-02 09:05:22 +0900

    update 216

--- solutions/216_combination_sum_III.py
+++ solutions/216_combination_sum_III.py
@@ -9,24 +9,21 @@ def main():
 
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        conbinations = []
+        results = []
 
-        def backtrack(start, path, remain):
-            if remain == 0 and len(path) == k:
-                conbinations.append(list(path))
-                return
-            elif remain < 0 or len(path) == k:
+        def backtrack(start, path, total):
+            if len(path) == k and total == n:
+                results.append(path.copy())
+            if len(path) >= k or total >= n:
                 return
 
             for i in range(start, 10):
                 path.append(i)
-                # Recursively call backtrack with the next number and updated remaining sum
-                backtrack(i+1, path, remain - i)
-                # Remove the last number from the path to backtrack
+                backtrack(i+1, path, total+i)
                 path.pop()
 
-        backtrack(1, [], n)
-        return conbinations
+        backtrack(1, [], 0)
+        return results
 
 
 if __name__ == '__main__':

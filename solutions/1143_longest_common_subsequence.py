5390a0b440b0c9e3f10ca493d909c841afeef1ac
masa08 <ushimasarug08@gmail.com>
2025-05-01 07:57:14 +0900

    update 1143

--- solutions/1143_longest_common_subsequence.py
+++ solutions/1143_longest_common_subsequence.py
@@ -8,17 +8,29 @@ def main():
 
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp_grid = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+        m, n = len(text1), len(text2)
+        dp = [[0] * (m+1) for _ in range(n+1)]
 
-        for col in reversed(range(len(text2))):
-            for row in reversed(range(len(text1))):
-                if text2[col] == text1[row]:
-                    dp_grid[row][col] = 1 + dp_grid[row + 1][col + 1]
+        for col in range(1, n+1):
+            for row in range(1, m+1):
+                if text2[col-1] == text1[row-1]:
+                    dp[col][row] = 1 + dp[col-1][row-1]
                 else:
-                    dp_grid[row][col] = max(
-                        dp_grid[row + 1][col], dp_grid[row][col + 1])
+                    dp[col][row] = max(dp[col-1][row], dp[col][row-1])
 
-        return dp_grid[0][0]
+        return dp[n][m]
+
+        # dp_grid = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+
+        # for col in reversed(range(len(text2))):
+        #     for row in reversed(range(len(text1))):
+        #         if text2[col] == text1[row]:
+        #             dp_grid[row][col] = 1 + dp_grid[row + 1][col + 1]
+        #         else:
+        #             dp_grid[row][col] = max(
+        #                 dp_grid[row + 1][col], dp_grid[row][col + 1])
+
+        # return dp_grid[0][0]
 
 
 if __name__ == '__main__':

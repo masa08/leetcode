9f4f62c8bd48123e184d109f7d15ea048bcf0f0a
masa08 <ushimasarug08@gmail.com>
2025-08-05 10:01:09 +0900

    update 1

--- solutions/1_two_sum.py
+++ solutions/1_two_sum.py
@@ -2,13 +2,21 @@ from typing import List
 
 
 def main():
-    nums = [2, 7, 11, 15]
-    target = 9
-
     solution = Solution()
-    result = solution.twoSum(nums, target)
 
-    print(result)
+    # 基本ケース
+    assert solution.twoSum([2, 7, 11, 15], 9) == [0, 1]
+    assert solution.twoSum([3, 2, 4], 6) == [1, 2]
+    assert solution.twoSum([3, 3], 6) == [0, 1]
+
+    # エッジケース
+    assert solution.twoSum([0, 4, 3, 0], 0) == [0, 3]
+    assert solution.twoSum([-1, -2, -3, -4, -5], -8) == [2, 4]
+
+    # 大きい数値
+    assert solution.twoSum([1000000000, 999999999, 1, 2], 1000000001) == [0, 2]
+
+    print("All tests passed!")
 
 
 class Solution:

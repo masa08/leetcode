7066771a27130f23ecc2a225e5f8dc13c0a9522f
masa08 <ushimasarug08@gmail.com>
2025-08-08 08:51:16 +0900

    update 242

--- solutions/242_valid_anagram.py
+++ solutions/242_valid_anagram.py
@@ -48,6 +48,9 @@ class Solution:
 
         # return True
 
+    def isAnagram2(self, s: str, t: str) -> bool:
+        return "".join(sorted(s)) == "".join(sorted(t))
+
 
 if __name__ == '__main__':
     main()

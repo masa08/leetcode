2eb5395564477ff495c01b14f125836be2d7eec8
masa08 <ushimasarug08@gmail.com>
2025-09-02 09:19:56 +0900

    add 71

---
+++ solutions/71_simplify_path.py
@@ -0,0 +1,31 @@
+def main():
+    solution = Solution()
+
+    assert solution.simplifyPath("/home/") == "/home"
+    assert solution.simplifyPath("/../") == "/"
+    assert solution.simplifyPath("/home//foo/") == "/home/foo"
+    assert solution.simplifyPath("/a/./b/../../c/") == "/c"
+
+    print("All tests passed!")
+
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        paths = [p for p in path.split('/') if p]
+        stack = []
+
+        for path in paths:
+            if path == '.':
+                continue
+            elif path == '..':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(path)
+
+        print(stack)
+        return '/' + '/'.join(stack)
+
+
+if __name__ == '__main__':
+    main()

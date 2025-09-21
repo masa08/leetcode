fccab7b486dc416d0e4a9737ac929048e780beba
masa08 <ushimasarug08@gmail.com>
2025-09-22 07:47:48 +0900

    update 1985

--- solutions/1985_find_kth_largest_integer_in_the_array.py
+++ solutions/1985_find_kth_largest_integer_in_the_array.py
@@ -3,11 +3,21 @@ from typing import List
 
 
 def main():
-    args = ["3", "6", "7", "10"]
-    k = 4
     solution = Solution()
-    result = solution.kthLargestNumber(args, k)
-    print(result)
+
+    # 基本ケース
+    assert solution.kthLargestNumber(["3", "6", "7", "10"], 4) == "3"
+    assert solution.kthLargestNumber(["2", "21", "12", "1"], 3) == "2"
+
+    # エッジケース
+    assert solution.kthLargestNumber(["0"], 1) == "0"
+    assert solution.kthLargestNumber(["1000000000"], 1) == "1000000000"
+
+    # 大きな数値
+    assert solution.kthLargestNumber(
+        ["683339452288515879", "7846081062003424420", "4805719838"], 2) == "683339452288515879"
+
+    print("All tests passed!")
 
 
 class Solution:

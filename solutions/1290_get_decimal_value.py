32958d0c147848f65867d4ddd08aae986e1ecaaa
masa08 <ushimasarug08@gmail.com>
2025-09-23 06:20:38 +0900

    update 1290

--- solutions/1290_get_decimal_value.py
+++ solutions/1290_get_decimal_value.py
@@ -3,21 +3,36 @@ from utils import makeLinkedList
 
 
 def main():
-    args = makeLinkedList([1, 0, 1])
     solution = Solution()
-    result = solution.getDecimalValue(args)
-    print(result)
+
+    # 基本ケース
+    assert solution.getDecimalValue(makeLinkedList([1, 0, 1])) == 5  # 101 = 5
+    assert solution.getDecimalValue(makeLinkedList([0])) == 0
+    assert solution.getDecimalValue(makeLinkedList([1])) == 1
+
+    # エッジケース
+    assert solution.getDecimalValue(makeLinkedList(
+        [1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0])) == 18880
+
+    print("All tests passed!")
 
 
 class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
-        binaryString = ""
+        # binaryString = ""
 
-        while head:
-            binaryString += str(head.value)
+        # while head:
+        #     binaryString += str(head.value)
+        #     head = head.next
+
+        # return int(binaryString, 2)
+
+        num = head.value
+        while head.next:
+            # Shift left by 1 (multiply by 2) and add next bit
+            num = (num << 1) | head.next.value
             head = head.next
-
-        return int(binaryString, 2)
+        return num
 
 
 if __name__ == '__main__':

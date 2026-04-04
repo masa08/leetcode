d64a12a61c1d5e7e32e4983ce23c71e014f698e4
masa08 <ushimasarug08@gmail.com>
2026-04-04 14:21:21 +0900

    add DoublyListNode class

--- model/list_node.py
+++ model/list_node.py
@@ -4,3 +4,12 @@ class ListNode(object):
     def __init__(self, value=0, next=None) -> None:
         self.value = value
         self.next = next
+
+
+class DoublyListNode:
+    """Node class for a doubly linked list."""
+
+    def __init__(self, val=0):
+        self.val = val
+        self.prev = None
+        self.next = None
